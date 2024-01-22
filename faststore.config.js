d89f58d23290aabafc224131900d24563bc555de
Helliton-Xavier <helliton.cruz@acct.global>
2024-01-22 15:45:10 -0300

    initial commit / develop created

--- faststore.config.js
+++ faststore.config.js
@@ -5,7 +5,7 @@ module.exports = {
     titleTemplate: "%s | FastStore",
     author: "Store Framework",
   },
-  theme: "custom-theme",
+  theme: "soft-blue",
   platform: "vtex",
   api: {
     storeId: "storeframework",
